80136f399ae0995972cef65d392e62f4b1d26ec9
khoa_pc <khoa@ubuntu.(none)>
2013-04-20 21:55:46 -0700

    app to test read and write process

---
+++ test_vf.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	char temp1, temp2;
+	int fd, i;
+
+	printf("opening the file /dev/vf-driver \n");
+	fd = open("/dev/vf-driver", O_RDWR);
+	if (fd < 0) {
+		printf("open /dev/vf-driver failed\n");
+		exit (-1);
+	} else printf("open /dev/vf-driver successfully\n");
+	temp1 = 'E';
+	i = write(fd, temp1, 1);
+	printf("number of char write = %d\n", i);
+
+	i = read(fd, temp2, 1);
+	printf("number of char read = %d\n", i);
+
+	if (i < 0) {
+		printf("read error\n");
+		close(fd);
+		exit (-1);
+	} else printf ("value read = %c\n", temp2);
+	close(fd);
+	exit (0);
+}
